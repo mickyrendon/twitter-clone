096bf4e4cead018e21055f9f2c501c135fce5133
micky rendon <elderendon@hotmail.es>
2022-10-12 18:54:57 -0300

    menu mobile modal done

--- js/app.js
+++ js/app.js
@@ -15,21 +15,30 @@ validator()
 // /////////////////////////////////////////////////////////
 // TODO:recorrer el array para clickear cualquier btn y abrir el menu
 const menuDots = document.querySelectorAll('.menu-dots-a')
+const modal =  document.querySelector('.modal')
+const menuOpts = document.querySelector('.opts-menu')
+
 menuDots.forEach((e)  => {
     e.addEventListener('click', checkStyle)
-
 })
 
+modal.addEventListener('click', () => {
+    modal.style.display = 'none'
+    menuOpts.style.display = 'none'
+})
 
 function checkStyle(){
-    const menuOpts = document.querySelector('.opts-menu')
+    const body = document.body
     const media = window.matchMedia("(max-width: 539px)").matches
     if( media && menuOpts.style.display === 'none'){
+        body.style.overflow = 'hidden'
+        modal.style.display = 'flex'
         return menuOpts.style.display = 'flex'
+
     }else{
         console.log('ventana muy grande para el menu, verlo en mobile');
         return menuOpts.style.display = 'none'
     }
 }
 
-window.onload = checkStyle
+window.onload = checkStyle, modal;
